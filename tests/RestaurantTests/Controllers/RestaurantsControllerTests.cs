f493b5e7bb5482a8e0a7bd06c6b2cd9e183ac2ad
Mostafa <mostafa.magdy22987@gmail.com>
2025-10-17 17:14:52 +0300

    Fix restaurant controller tests

--- tests/RestaurantTests/Controllers/RestaurantsControllerTests.cs
+++ tests/RestaurantTests/Controllers/RestaurantsControllerTests.cs
@@ -5,8 +5,12 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Moq;
+using Restaurant.Application.Restaurants.DTOs;
+using Restaurant.Domain.Entities;
+using Restaurant.Domain.Repositories;
 using Restaurant.Inftastructure.Seeders;
 using RestaurantTests.Controllers;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -15,6 +19,7 @@ namespace Restaurant.Controllers.Tests
     public class RestaurantsControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly WebApplicationFactory<Program> _applicationFactory;
+        private readonly Mock<IRestaurantRepository> _restaurantRepositoryMock = new();
         private readonly Mock<IRestaurantSeeder> _restaurantSeederMock = new();
         public RestaurantsControllerTests(WebApplicationFactory<Program> factory)
         {
@@ -23,6 +28,8 @@ namespace Restaurant.Controllers.Tests
                 builder.ConfigureTestServices(services =>
                 {
                     services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>(); //injecting the fake policy evaluator
+
+                    services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantRepository), _ => _restaurantRepositoryMock.Object));
 
                     services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantSeeder), _ => _restaurantSeederMock.Object));
                 });
@@ -59,8 +66,9 @@ namespace Restaurant.Controllers.Tests
         public async Task GetById_ForInValidIdRequest_ShouldReturnNotFound() //we have to make a fake policy evaluator in order to skip authorization for this endpoint
         {
             //arrange 
-            var client = _applicationFactory.CreateClient();
             var id = 123;
+            _restaurantRepositoryMock.Setup(m => m.GetById(id)).ReturnsAsync((Restaurantt?) null); //mocking the repository to return null for the given id
+            var client = _applicationFactory.CreateClient();
 
             //act
             var result = await client.GetAsync($"api/Restaurants/{id}");
@@ -75,12 +83,23 @@ namespace Restaurant.Controllers.Tests
             //arrange 
             var client = _applicationFactory.CreateClient();
             var id = 1;
+            var restaurant = new Restaurantt()
+            {
+                Id = id,
+                Name = "Test",
+                Description = "Test",
+            };
+            _restaurantRepositoryMock.Setup(m => m.GetById(id)).ReturnsAsync(restaurant);
 
             //act
             var result = await client.GetAsync($"api/Restaurants/{id}");
+            var restaurantDto = await result.Content.ReadFromJsonAsync<RestaurantsDTO>();
 
             //assert
             result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            restaurantDto.Should().NotBeNull();
+            restaurantDto.Name.Should().Be("Test");
+            restaurantDto.Description.Should().Be("Test");
         }
     }
 }
